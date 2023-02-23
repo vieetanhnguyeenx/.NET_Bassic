4110ed55bbf1efc6562b283473469fa1e990e5a9
NVA <vieetanhnguyeenx@gmail.com>
2023-02-23 11:06:16 +0700

    Slide 8

--- Winform/Slide_8/Slide_8/Form1.Designer.cs
+++ Winform/Slide_8/Slide_8/Form1.Designer.cs
@@ -28,12 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lstDanhSach = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnCount = new System.Windows.Forms.Button();
+            this.btnIndex = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstDanhSach
+            // 
+            this.lstDanhSach.FormattingEnabled = true;
+            this.lstDanhSach.ItemHeight = 15;
+            this.lstDanhSach.Items.AddRange(new object[] {
+            "Nguyen Van A",
+            "Nguyen Van B"});
+            this.lstDanhSach.Location = new System.Drawing.Point(79, 132);
+            this.lstDanhSach.Name = "lstDanhSach";
+            this.lstDanhSach.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.lstDanhSach.Size = new System.Drawing.Size(171, 124);
+            this.lstDanhSach.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(466, 46);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnCount
+            // 
+            this.btnCount.Location = new System.Drawing.Point(466, 105);
+            this.btnCount.Name = "btnCount";
+            this.btnCount.Size = new System.Drawing.Size(75, 23);
+            this.btnCount.TabIndex = 2;
+            this.btnCount.Text = "Count";
+            this.btnCount.UseVisualStyleBackColor = true;
+            this.btnCount.Click += new System.EventHandler(this.btnCount_Click);
+            // 
+            // btnIndex
+            // 
+            this.btnIndex.Location = new System.Drawing.Point(466, 162);
+            this.btnIndex.Name = "btnIndex";
+            this.btnIndex.Size = new System.Drawing.Size(75, 23);
+            this.btnIndex.TabIndex = 3;
+            this.btnIndex.Text = "Index";
+            this.btnIndex.UseVisualStyleBackColor = true;
+            this.btnIndex.Click += new System.EventHandler(this.btnIndex_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnIndex);
+            this.Controls.Add(this.btnCount);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lstDanhSach);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private ListBox lstDanhSach;
+        private Button btnAdd;
+        private Button btnCount;
+        private Button btnIndex;
     }
 }
--- Winform/Slide_8/Slide_8/Form1.cs
+++ Winform/Slide_8/Slide_8/Form1.cs
@@ -6,5 +6,20 @@ namespace Slide_8
         {
             InitializeComponent();
         }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            lstDanhSach.Items.Add("Nguyen Van C");
+        }
+
+        private void btnCount_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("So phan tu " + lstDanhSach.Items.Count);
+        }
+
+        private void btnIndex_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(lstDanhSach.Items[0].ToString());
+        }
     }
 }
